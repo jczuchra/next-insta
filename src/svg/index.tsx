5b19c071e898ca9cf445ea106fa85403c1432d94
jczuchra <czuchra.jakub@gmail.com>
2022-05-25 18:48:09 +0200

    Add svgs

---
+++ src/svg/index.tsx
@@ -0,0 +1,43 @@
+import CommentOutline from './CommentOutline';
+import DirectOutline from './DirectOutline';
+import DirectSolid from './DirectSolid';
+import EmojiOutline from './EmojiOutline';
+import ExploreOutline from './ExploreOutline';
+import ExploreSolid from './ExploreSolid';
+import HeartOutline from './HeartOutline';
+import HeartSolid from './HeartSolid';
+import HomeOutline from './HomeOutline';
+import HomeSolid from './HomeSolid';
+import PostOutline from './PostOutline';
+import PostsOutline from './PostsOutline';
+import ProfileOutline from './ProfileOutline';
+import ReelsOutline from './ReelsOutline';
+import SaveOutline from './SaveOutline';
+import SaveSolid from './SaveSolid';
+import SettingsOutline from './SettingsOutline';
+import SwitchOutline from './SwitchOutline';
+import TaggedOutline from './TaggedOutline';
+import VideosOutline from './VideosOutline';
+
+export {
+  CommentOutline,
+  DirectOutline,
+  DirectSolid,
+  EmojiOutline,
+  ExploreOutline,
+  ExploreSolid,
+  HeartOutline,
+  HeartSolid,
+  HomeOutline,
+  HomeSolid,
+  PostOutline,
+  PostsOutline,
+  ProfileOutline,
+  ReelsOutline,
+  SaveOutline,
+  SaveSolid,
+  SettingsOutline,
+  SwitchOutline,
+  TaggedOutline,
+  VideosOutline,
+};
